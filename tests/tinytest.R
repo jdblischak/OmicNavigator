eac77788e902dc6c24b52345d881193833a7e47b
John Blischak <jdblischak@gmail.com>
2025-05-19 13:21:37 -0400

    Make test_package() controllable by env var TT_AT_HOME

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,8 @@
 # Test files in inst/tinytest/
+
+# Make test_package() controllable by env var TT_AT_HOME
+home <- identical(Sys.getenv("TT_AT_HOME"), "TRUE")
+
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  suppressMessages(tinytest::test_package("OmicNavigator"))
+  suppressMessages(tinytest::test_package("OmicNavigator", at_home = home))
 }
